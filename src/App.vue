de9bfd9ab7beefae3e99ee6110f364e5230ac031
web-hefeng <913462479@qq.com>
2022-02-17 22:59:32 +0800

    增加ArcGIS js API 实例化地图

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,47 @@
 <template>
   <div id="app">
-    
+    <el-container class="app-out-panel">
+      <el-header class="sys-header">系统标题</el-header>
+      <el-container class="app-context-panel">
+        <el-aside class="left-aside" width='200px'>左侧菜单</el-aside>
+        <el-main class="sys-content">
+          <Mapview />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-
+import Mapview from './components/common/Mapview';
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    
-  }
-}
+    Mapview
+  },
+};
 </script>
 
 <style>
-html,body,#app {
+html,
+body,
+#app {
   position: relative;
   width: 100%;
   height: 100%;
-  margin: 0 !important;
+  margin: 0;
+}
+.app-out-panel,.app-context-panel{
+  height: 100%;
+}
+.sys-header{
+  background-color: blue;
+}
+.left-aside{
+  background-color:gray;
+}
+.sys-content {
+  margin: 0;
+  padding: 0;
 }
 </style>
